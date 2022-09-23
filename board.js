985c9feb52d96b79e86e29343cd952637c6c674d
Duke <dukefreigns@gmail.com>
2022-09-23 15:26:48 -0800

    Add physical board with styles

--- board.js
+++ board.js
@@ -1,19 +1,26 @@
 const board = document.createElement(`div`);
-let gridSize = 4;
-board.classList.add(`.board`);
+let subjectGrid = [];
+let gridSize = 16;
+board.classList.add(`board`);
 
 
 for (let i = 0; i < gridSize; i++) {
     let row = document.createElement('div');
-    row.classList.add(`.row`);
+    row.classList.add(`row`);
 
     for (let i = 0; i < gridSize; i++) {
         let subject = document.createElement(`div`);
-        subject.classList.add(`.subject`);
+        subject.classList.add(`subject`);
+        subject.addEventListener(`mouseover`, shiftToblack);
         row.appendChild(subject);
+        subjectGrid.push(subject);
     }
 
     board.appendChild(row);
 }
 
 document.body.appendChild(board);
+
+function shiftToblack() {
+    this.classList.add(`hover`);
+}
